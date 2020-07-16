736a4be35b5aac9a7af5ccac70d52b06d80e0dd8
Anderson Barroso <andersonsdb25@hotmail.com>
2020-07-16 20:04:56 -0300

    Entidades Criadas

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,16 @@
+package aplicacao;
+
+import entidades.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Departamento departamento = new Departamento(1, "Livros");
+		
+		System.out.println(departamento);
+		
+		
+	}
+
+}
