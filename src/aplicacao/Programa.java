199629cb61d7e68fa942524a0180d675a0372b51
Anderson Barroso <andersonsdb25@hotmail.com>
2020-07-16 20:26:20 -0300

    Correcoes Entidades

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,16 +1,19 @@
 package aplicacao;
 
+import java.util.Date;
+
 import entidades.Departamento;
+import entidades.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
 
 		Departamento departamento = new Departamento(1, "Livros");
-		
+		Vendedor vendedor = new Vendedor(21, "Michael", "michael@hotmail.com", new Date(), 5000.0, departamento);
 		System.out.println(departamento);
-		
-		
+		System.out.println(vendedor);
+
 	}
 
 }
--- src/entidades/Vendedor.java
+++ src/entidades/Vendedor.java
@@ -1,8 +1,11 @@
 package entidades;
 
-import java.sql.Date;
+import java.io.Serializable;
+import java.util.Date;
 
-public class Vendedores {
+public class Vendedor implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private Integer id;
 	private String nome;
@@ -11,7 +14,12 @@ public class Vendedores {
 	private Double salarioBase;
 	private Departamento departamento;
 	
-	public Vendedores(Integer id, String nome, String email, Date dataNasc, Double salarioBase,
+	
+	
+	public Vendedor() {
+	}
+	
+	public Vendedor(Integer id, String nome, String email, Date dataNasc, Double salarioBase,
 			Departamento departamento) {
 		this.id = id;
 		this.nome = nome;
@@ -67,6 +75,31 @@ public class Vendedores {
 
 	public void setDepartamento(Departamento departamento) {
 		this.departamento = departamento;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vendedor other = (Vendedor) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	@Override
